d88c3f698dd037c8e9bb795d7e8c067dccd46b74
Daniel Doza <doza.daniel@gmail.com>
2019-01-17 17:38:50 +0100

    refactore point done - delete the old points interface

--- point/point.go
+++ point/point.go
@@ -11,12 +11,6 @@ type Point struct {
 	Y float64 `json:"y"`
 }
 
-// Points ...
-type Points struct {
-	Pts  []Point
-	less func(Point, Point) bool
-}
-
 type sortable struct {
 	pts  []Point
 	less func(Point, Point) bool
@@ -55,55 +49,6 @@ func Count(pts []Point, f func(Point) bool) int {
 		}
 	}
 	return cnt
-}
-
-// Sort ...
-func (p Points) Sort(f func(Point, Point) bool) {
-	p.less = f
-	sort.Sort(p)
-	p.less = nil
-}
-
-// Find ...
-func (p Points) Find(f func(Point) bool) int {
-	if f == nil {
-		return -1
-	}
-
-	for i, pt := range p.Pts {
-		if f(pt) {
-			return i
-		}
-	}
-	return -1
-}
-
-// Count ...
-func (p Points) Count(f func(Point) bool) int {
-	if f == nil {
-		return 0
-	}
-
-	cnt := 0
-	for _, pt := range p.Pts {
-		if f(pt) {
-			cnt++
-		}
-	}
-	return cnt
-}
-
-func (p Points) Len() int {
-	return len(p.Pts)
-}
-func (p Points) Swap(i, j int) {
-	p.Pts[i], p.Pts[j] = p.Pts[j], p.Pts[i]
-}
-func (p Points) Less(i, j int) bool {
-	if p.less == nil {
-		return false
-	}
-	return p.less(p.Pts[i], p.Pts[j])
 }
 
 func (s sortable) Len() int {
