94e67564628e48e446c34781723743e17bf1bf75
Daniel Doza <doza.daniel@gmail.com>
2019-01-17 17:31:15 +0100

    start refactor on point.go - replace methods for Points structure

--- point/point.go
+++ point/point.go
@@ -17,6 +17,46 @@ type Points struct {
 	less func(Point, Point) bool
 }
 
+type sortable struct {
+	pts  []Point
+	less func(Point, Point) bool
+}
+
+// Sort ...
+func Sort(pts []Point, less func(Point, Point) bool) {
+	s := sortable{pts, less}
+	sort.Sort(s)
+}
+
+// Find ...
+func Find(pts []Point, f func(Point) bool) int {
+	if f == nil {
+		return -1
+	}
+
+	for i, pt := range pts {
+		if f(pt) {
+			return i
+		}
+	}
+	return -1
+}
+
+// Count ...
+func Count(pts []Point, f func(Point) bool) int {
+	if f == nil {
+		return 0
+	}
+
+	cnt := 0
+	for _, pt := range pts {
+		if f(pt) {
+			cnt++
+		}
+	}
+	return cnt
+}
+
 // Sort ...
 func (p Points) Sort(f func(Point, Point) bool) {
 	p.less = f
@@ -64,6 +104,21 @@ func (p Points) Less(i, j int) bool {
 		return false
 	}
 	return p.less(p.Pts[i], p.Pts[j])
+}
+
+func (s sortable) Len() int {
+	return len(s.pts)
+}
+
+func (s sortable) Swap(i, j int) {
+	s.pts[i], s.pts[j] = s.pts[j], s.pts[i]
+}
+
+func (s sortable) Less(i, j int) bool {
+	if s.less == nil {
+		return false
+	}
+	return s.less(s.pts[i], s.pts[j])
 }
 
 func (p Point) String() string {
