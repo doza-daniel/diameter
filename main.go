a0d8ffbbd5c8683725ed9919428662ab048e0401
Daniel Doza <doza.daniel@gmail.com>
2019-01-23 09:47:49 +0100

    final part of the algorithm added: seems to work

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"os"
 
 	"github.com/doza-daniel/diameter/convexhull"
+	"github.com/doza-daniel/diameter/line"
 	"github.com/doza-daniel/diameter/point"
 )
 
@@ -40,6 +41,31 @@ func main() {
 }
 
 func diameter(points []point.Point) (point.Point, point.Point, error) {
-	_ = convexhull.ConvexHull(points)
-	return point.Point{}, point.Point{}, nil
+	type candidate struct {
+		a, b point.Point
+	}
+	hull := convexhull.ConvexHull(points)
+	candidates := make([]candidate, 0)
+	last := 2
+	for i, vertex := range hull {
+		edge := line.ThroughPoints(vertex, hull[(i+1)%len(hull)])
+		for {
+			next := (last + 1) % len(hull)
+			dLast := edge.DistanceFromPoint(hull[last])
+			dNext := edge.DistanceFromPoint(hull[next])
+			if dNext < dLast {
+				candidates = append(candidates, candidate{vertex, hull[last]})
+				break
+			}
+			last = next
+		}
+	}
+
+	max := candidates[0]
+	for _, c := range candidates[1:] {
+		if point.Distance(c.a, c.b) > point.Distance(max.a, max.b) {
+			max = c
+		}
+	}
+	return max.a, max.b, nil
 }
